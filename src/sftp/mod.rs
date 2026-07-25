05fef0a8a8e6b09fee5c2aec45c184a6e6a0c074
pengjuewen <2661843290@qq.com>
2026-07-25 17:46:14 +0800

    refactor: Remove unused structures and imports from session and resource modules

--- src/sftp/mod.rs
+++ src/sftp/mod.rs
@@ -1,7 +1,3 @@
 #[path = "impls/sftp_impl.rs"]
 mod sftp_impl;
-#[path = "struct/sftp_struct.rs"]
-mod sftp_struct;
-
 pub(crate) use sftp_impl::*;
-pub(crate) use sftp_struct::{SftpHandles, SftpLastCwd};
